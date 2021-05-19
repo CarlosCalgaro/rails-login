8f4f60b5d68b3aa2bf3c1fae290957783ae14d8e
Carlos Calgaro <carloscalgarof@gmail.com>
2021-05-19 21:40:03 +0100

    Refactoring the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,37 +1,47 @@
-class SessionsController < ApplicationController
-  
-  skip_before_action :authenticate_user!, only: [:new, :create]
+# frozen_string_literal: true
 
+class SessionsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[new create]
+  before_action :check_if_already_logged_in, only: :new
+  before_action :set_user, only: :create
+  
   def new
   end
 
   def create
-    user = User.find_by(username: params[:username])
-
-    if user && !user.locked && user.authenticate(params[:password])
-      session[:current_user_id] = user.id
-      flash[:notice] = "You have logged in sussessfully"
-      redirect_to root_url
-    elsif user.locked?
-      flash.now[:alert] = "Account locked. Contact your administrator!"
-      render :new
-    else
-      Authentication::FailedLoginService::perform(user) unless user.nil?
-      flash.now[:alert] = "Invalid username or password!"
+    authentication = Authentication::LoginService.new(@user, params[:password]).call()
+    @user = authentication.user
+    if authentication.success?
+      session[:current_user_id] = @user.id
+      redirect_to root_path
+    else      
+      flash[:alert] = authentication.message
       render :new
     end
   end
 
-  def update
-  end
-
   def destroy
+    session[:current_user_id] = nil
+    redirect_to sessions_new_path
   end
 
   private
 
+  
+
   def user_params
     params.permit(:user).require(:email)
+  end
+  
+  def set_user
+    @user||= User.find_by(username: params[:username])
+  end
+  
+  def check_if_already_logged_in
+    if current_user
+      flash[:notice] = t('alerts.already_logged_in')
+      redirect_to root_url
+    end
   end
 
 end
