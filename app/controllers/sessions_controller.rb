827866ca99bf123f7d833210759484a363fde75c
Carlos Calgaro <carloscalgarof@gmail.com>
2021-05-19 08:30:47 +0100

    Added login_attempts to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,24 @@
 class SessionsController < ApplicationController
   
+  skip_before_action :authenticate_user!, only: [:new, :create]
+
   def new
   end
 
   def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:current_user_id] = user.id
+      flash[:notice] = "You have logged in sussessfully"
+      redirect_to root_url
+    elsif user.nil?
+      flash.now[:alert] = "User not found!"
+      render :new
+    else 
+      user.update(login_attempts: user.login_attempts + 1)
+      flash.now[:alert] = "Invalid username or password!"
+      render :new
+    end
   end
 
   def update
@@ -11,4 +26,11 @@ class SessionsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def user_params
+    params.permit(:user).require(:email)
+  end
+
 end
---
+++ db/migrate/20210518233200_add_login_attempts_and_to_users.rb
@@ -0,0 +1,8 @@
+class AddLoginAttemptsAndToUsers < ActiveRecord::Migration[6.1]
+  
+  def change
+  
+    add_column :users, :login_attempts, :integer, default: 0
+  end
+
+end
