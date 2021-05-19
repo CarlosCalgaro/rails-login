24f39c768677988431eeec497dc143e94288c523
Carlos Calgaro <carloscalgarof@gmail.com>
2021-05-19 21:39:39 +0100

    Added authentication methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,20 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
 
-    before_filter :authenticate_user!
+  def current_user
+    @current_user ||= User.find_by(id: session[:current_user_id])
+  end
 
+  private
 
-    def authenticate_user!
-       redirect_to sessions_new_path if @current_user.nil?
+  def authenticate_user!
+    if current_user && current_user.locked?
+      flash.now[:alert] = t('activerecord.errors.models.user.attributes.locked')
+      redirect_to sessions_new_path
+    elsif !current_user
+      redirect_to sessions_new_path
     end
-
-    private
-
-    def current_user
-        @current_user ||= User.find_by(id: session[:current_user_id])
-    end
-
+  end
 end
