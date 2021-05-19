f1032b1bb3e90a9601a786eccf989e82246acf1c
Carlos Calgaro <carloscalgarof@gmail.com>
2021-05-19 10:50:29 +0100

    Fixed a bug when validating password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true, length: {in: 6..15}
     validates :email, presence: true, uniqueness: true
-
-    validates :password, length: { in: 6..20 }
     validates :password, presence: true, on: :create
-    validates :password, confirmation: true, if: :password_present?
+    validates :password, confirmation: true, length: { in: 6..20 }, if: :password_present?
     validates :password_confirmation, presence: true, if: :password_present?
 
     before_save :set_password_digest
@@ -16,8 +14,7 @@ class User < ApplicationRecord
     def authenticate(password)
         BCrypt::Password.new(password_digest).is_password?(password)
     end
-
-
+    
     private
 
     def set_password_digest
