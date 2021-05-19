6546f223f28092a35eb1e0c9821fe7ce0c8fe3d8
Carlos Calgaro <carloscalgarof@gmail.com>
2021-05-19 21:47:27 +0100

    Removed unused email field

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   attr_accessor :password
 
   validates :username, presence: true, uniqueness: true, length: { in: 6..15 }
-  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, on: :create
   validates :password, confirmation: true, length: { in: 6..20 }, if: :password_present?
   validates :password_confirmation, presence: true, if: :password_present?
