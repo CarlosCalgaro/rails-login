ac96720a2bcd822ac7be969984e2e5131aec4929
Carlos Calgaro <carloscalgarof@gmail.com>
2021-05-19 10:33:57 +0100

    Added user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,32 @@
 class User < ApplicationRecord
-    has_secure_password
 
-    validates :username, presence: true, uniqueness: true
+    attr_accessor :password
+
+    validates :username, presence: true, uniqueness: true, length: {in: 6..15}
     validates :email, presence: true, uniqueness: true
+
+    validates :password, length: { in: 6..20 }
+    validates :password, presence: true, on: :create
+    validates :password, confirmation: true, if: :password_present?
+    validates :password_confirmation, presence: true, if: :password_present?
+
+    before_save :set_password_digest
+
+    
+    def authenticate(password)
+        BCrypt::Password.new(password_digest).is_password?(password)
+    end
+
+
+    private
+
+    def set_password_digest
+        return if password.nil? || password_confirmation.nil?
+        self.password_digest = BCrypt::Password.create(password)
+    end
+    
+    def password_present?
+        !password.blank?
+    end
+
 end
