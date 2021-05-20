3fb9a12f9f88cbb798f6535082823f7155f117eb
Carlos Calgaro <carloscalgarof@gmail.com>
2021-05-20 01:58:26 +0100

    Added user specs and factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -9,7 +9,33 @@ FactoryBot.define do
       locked { true }
     end
 
+    trait :short_named do 
+      username {'short'}
+    end
 
+    trait :short_password do 
+      password {'1234'}
+      password_confirmation {'1234'}
+    end
+    
+    trait :long_password do 
+      password {'1234567890qwerty12345'}
+      password_confirmation {'1234567890qwerty12345'}
+    end
+    
+    trait :long_named do 
+      username {'ThisIsaBigNameForaUser'}
+    end
+
+    trait :nameless do 
+      username {nil}
+    end
   factory :locked_user, traits: [:locked]
+  factory :long_named_user, traits: [:long_named]
+  factory :short_named_user, traits: [:short_named]
+  factory :nameless_user, traits: [:nameless]
+  factory :short_password_user, traits: [:short_password]
+  factory :long_password_user, traits: [:long_password]
+
   end
 end
